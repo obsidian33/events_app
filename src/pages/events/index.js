6d7b6b8f07e25f61a6eeb2c1ea6a4bd97bb3fdae
obsidian33 <6493594+obsidian33@users.noreply.github.com>
2024-04-25 23:00:53 -0700

    basic pages

---
+++ src/pages/events/index.js
@@ -0,0 +1,21 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Events Page</h1>
+      <a href="">
+        <img />
+        <h2>Events in London</h2>
+      </a>
+      <a href="">
+        <img />
+        <h2>Events in San Francisco</h2>
+      </a>
+      <a href="">
+        <img />
+        <h2>Events in Barcelona</h2>
+      </a>
+    </div>
+  )
+}
+
+export default Page;
